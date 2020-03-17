95418e93f8f75ad31a0527e2750955c65d48010e
Pavlo Kantsir <34620231+Pashkaua@users.noreply.github.com>
2020-03-17 17:14:57 +0200

    test prettier

--- src/components/AddPet.tsx
+++ src/components/AddPet.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 
 type AddPetProps<T = any> = {
-    name: string;
-    type: T;
+  name: string;
+  type: T;
 };
 
 export const AddPet: React.FC<AddPetProps> = props => {
-    return (
-        <div>
-            <h1>Add new Pet</h1>
-        </div>
-    );
+  return (
+    <div>
+      <h1>Add new Pet</h1>
+    </div>
+  );
 };
