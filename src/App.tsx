319e65dfedb61307c510873583513e83632d7e7f
Pavlo Kantsir <34620231+Pashkaua@users.noreply.github.com>
2020-03-17 07:34:14 +0200

    returned deleted file(logo.svg) to fix the bug

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
+import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-
 import Home from './components/Home';
 import Auth from './components/Auth';
 import Registration from './components/Registration';
@@ -14,20 +14,30 @@ import Comparison from './components/Comparison';
 
 function App() {
   return (
-    <Router>
-      <Switch>
-        <Route exact path="/" component={Home}></Route>
-        <Route exact path="/auth" component={Auth}></Route>
-        <Route exact path="/registration" component={Registration}></Route>
-        <Route exact path="/search" component={Search}></Route>
-        <Route exact path="/result" component={Result}></Route>
-        <Route exact path="/user" component={User}></Route>
-        <Route exact path="/pet" component={Pet}></Route>
-        <Route exact path="/addpet" component={AddPet}></Route>
-        <Route exact path="/comparison" component={Comparison}></Route>
-      </Switch>
-    </Router>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+          Learn React
+        </a>
+      </header>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Home}></Route>
+          <Route exact path="/auth" component={Auth}></Route>
+          <Route exact path="/registration" component={Registration}></Route>
+          <Route exact path="/search" component={Search}></Route>
+          <Route exact path="/result" component={Result}></Route>
+          <Route exact path="/user" component={User}></Route>
+          <Route exact path="/pet" component={Pet}></Route>
+          <Route exact path="/addpet" component={AddPet}></Route>
+          <Route exact path="/comparison" component={Comparison}></Route>
+        </Switch>
+      </Router>
+    </div>
   );
 }
-
 export default App;
