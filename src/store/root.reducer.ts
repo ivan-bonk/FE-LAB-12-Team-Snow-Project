508cd1c219dfe4c491732f73219d8a51a5637b46
Pavlo Kantsir <34620231+Pashkaua@users.noreply.github.com>
2020-03-31 13:42:51 +0300

    remove comment

--- src/store/root.reducer.ts
+++ src/store/root.reducer.ts
@@ -1,26 +1,3 @@
-// const initiatState = {
-//   data: {},
-//   petsToCompare: [],
-//   filter: {},
-// };
-
-// export const rootReducer = (state: object = initiatState, action: any) => {
-//   switch (action.type) {
-//     case 'PUT_PETS':
-//       return {
-//         ...state,
-//         petsToCompare: action.payload.pets,
-//       };
-//     case 'FILTER':
-//       return {
-//         ...state,
-//         filter: action.payload,
-//       };
-//   }
-
-//   return state;
-// };
-
 import { createReducer } from 'typesafe-actions';
 
 type State = {} | [];
