05c7ae010af598070b3883f6a4390e13103c7d7b
Ivan Bonk <44017234+IvanBonk@users.noreply.github.com>
2020-04-01 19:30:03 +0300

    fix travis braking

--- src/pages/comparison/interfaces/pet.interface.ts
+++ src/pages/comparison/interfaces/pet.interface.ts
@@ -1,8 +1,45 @@
-export interface Pet {
-  _id: string;
+export interface PetsToCompareList {
+  name: string;
+  pets: PetToCompare[];
+}
+
+export interface PetToCompare {
+  id: string;
+  breed: string;
   imgUrl: string;
-  alias: string;
-  popularity: number;
-  training: number;
-  careTime: string;
+  value: number;
+  textValue?: string;
+  type: boolean;
+}
+
+export interface ProfileProps {
+  breed: string;
+  imgUrl: string;
+}
+
+export interface RateProps {
+  rate: number;
+}
+
+export type ComparisonProps = {
+  name: string;
+  match: {
+    params: {
+      ids: string;
+    };
+  };
+};
+
+export interface RootState {
+  comparison: ComparisonState;
+}
+
+export interface ComparisonState {
+  petsToCompare: Array<PetsToCompareList>;
+  loading: boolean;
+  error: any;
+}
+
+export interface CharacteristicProps {
+  data: PetsToCompareList;
 }
