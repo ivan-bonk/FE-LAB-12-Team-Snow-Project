032d56e3f3d2a62eb7590d336cf5ba56bf8a744d
Danyil <dkichalo@gmail.com>
2020-04-01 21:24:42 +0300

    Change ' singlequote to doublequote

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,7 +16,7 @@ module.exports = {
       // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
       // e.g. "@typescript-eslint/explicit-function-return-type": "off",
       "@typescript-eslint/explicit-function-return-type": 0,
-      'react/prop-types': 0,
+      "react/prop-types": 0,
     },
     settings: {
       react: {
