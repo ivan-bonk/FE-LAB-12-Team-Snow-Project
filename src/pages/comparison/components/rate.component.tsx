e83107c62ba50f7b35acfcbf5ff1e7b4eef4e7f4
Beker94 <bekerskyi@gmail.com>
2020-04-01 15:20:06 +0300

    create landing page

--- src/pages/comparison/components/rate.component.tsx
+++ src/pages/comparison/components/rate.component.tsx
@@ -2,30 +2,26 @@ import React from 'react';
 import './rate.css';
 
 export const Rate = (props: { rate: number }) => {
-    const { rate } = props;
+  const { rate } = props;
 
-    let clases: string[] = ['gy', 'gy', 'gy', 'gy', 'gy']; // gy - grey
+  const clases: string[] = ['gy', 'gy', 'gy', 'gy', 'gy']; // gy - grey
 
-    if (rate === 1) {
-        clases[0] = 'r';                  // r - red
-    } else if (rate < 4) {
-        for (let i = 0; i < rate; i++) {
-            clases[i] = 'y';              // y - yellow
-        }
-    } else {
-        for (let i = 0; i < rate; i++) {
-            clases[i] = 'gr';             // gr - green
-        }
+  if (rate === 1) {
+    clases[0] = 'r'; // r - red
+  } else if (rate < 4) {
+    for (let i = 0; i < rate; i++) {
+      clases[i] = 'y'; // y - yellow
     }
+  } else {
+    for (let i = 0; i < rate; i++) {
+      clases[i] = 'gr'; // gr - green
+    }
+  }
 
-    let key: number = 0;
-    const items = clases.map((el) => {
-        return <div key={key++} className={el}></div>;
-    })
+  let key = 0;
+  const items = clases.map(el => {
+    return <div key={key++} className={el}></div>;
+  });
 
-    return (
-        <div className="rate-bar">
-            {items}
-        </div>
-    );
-}
+  return <div className="rate-bar">{items}</div>;
+};
---
+++ src/pages/landing-page/components/find-out/find-out.component.tsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import img1 from '../../../../images/Landing 3.png';
+import dog from './bigl.svg';
+import styles from './find-out.module.scss';
+
+export const FindOut: React.FC<{}> = () => {
+  return (
+    <div className={styles.block}>
+      <h2 className={styles.block__title}>Дізнайтеся чи правильно ви доглядаєте за соїм улюбленцем</h2>
+      <img src={img1} width="200" className={styles.block__img}></img>
+
+      <table className={styles.table}>
+        <thead className={styles.table__head}>
+          <tr>
+            <th className={styles.table__row1}>
+              <img src={dog} />
+              <span className={styles.table__name}>Бігль</span>
+            </th>
+            <th className={styles.table__row2}>Дані</th>
+            <th className={styles.table__row3}>Норма</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td className={styles.table__row1}>Вага, кг</td>
+            <td className={(styles.table__data, styles.table__data_middle)}>20</td>
+            <td>25 - 30</td>
+          </tr>
+          <tr>
+            <td className={styles.table__row1}>Довжина, см</td>
+            <td className={(styles.table__data, styles.table__data_good)}>30</td>
+            <td>27 - 33</td>
+          </tr>
+          <tr>
+            <td className={styles.table__row1}>Кілкість вигулювань вдень</td>
+            <td className={(styles.table__data, styles.table__data_good)}>4</td>
+            <td>3-5</td>
+          </tr>
+          <tr>
+            <td className={styles.table__row1}>Кількість прийомів їжі за день</td>
+            <td className={(styles.table__data, styles.table__data_bad)}>5</td>
+            <td>2-4</td>
+          </tr>
+        </tbody>
+      </table>
+      <Link to={`/`} className={styles.block__link}>
+        Дізнатися →
+      </Link>
+    </div>
+  );
+};
