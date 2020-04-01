de540d9b805d754687d7ca9e4a09627c1e9eda7b
Danyil <dkichalo@gmail.com>
2020-04-01 21:17:24 +0300

    Add prop-types rule to eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,13 +15,12 @@ module.exports = {
     rules: {
       // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
       // e.g. "@typescript-eslint/explicit-function-return-type": "off",
-      'react/prop-types': 0,
       "@typescript-eslint/explicit-function-return-type": 0,
+      'react/prop-types': 0,
     },
     settings: {
       react: {
         version: 'detect', // Tells eslint-plugin-react to automatically detect the version of React to use
-
       },
     },
   },
