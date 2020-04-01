f791f292edcd7550760b698cde6af085540c868e
Beker94 <bekerskyi@gmail.com>
2020-04-01 16:59:25 +0300

    create landing page

---
+++ src/pages/landing-page/components/to-pick-up/to-pick-up.component.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import img2 from '../../../../images/Landing 2.png';
+import dialogs from '../../../../images/dialog.svg';
+import styles from './to-pick-up.module.scss';
+
+export const ToPickUp: React.FC<{}> = () => {
+  return (
+    <div className={styles.block}>
+      <h2 className={styles.block__title}>Підберіть свого ідеального собаку</h2>
+      <img src={img2} width="200" className={styles.block__img}></img>
+      <img src={dialogs} className={styles.block__img}></img>
+      <Link to={`/`} className={styles.block__link}>
+        Підібрати →
+      </Link>
+    </div>
+  );
+};
