aa1ac0897eadff7038e0354561e4681b3d64d250
Pavlo Kantsir <34620231+Pashkaua@users.noreply.github.com>
2020-04-02 10:43:06 +0300

    remove user route and page (unused)

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,6 @@ import { ROUTES } from './shared/constants/routes.constants';
 import { LandinPage } from './pages/landing-page/landing-page.component';
 import { Search } from './pages/Search';
 import { Result } from './pages/result/result.component';
-import { User } from './pages/User';
 import { Pet } from './pages/pet/pet.component';
 import { AddPet } from './pages/AddPet';
 import { Comparison } from './pages/comparison/comparison.component';
@@ -19,7 +18,6 @@ function App() {
         <Route exact path={ROUTES.home} component={LandinPage}></Route>
         <Route path={ROUTES.search} component={Search}></Route>
         <Route path={ROUTES.result} component={Result}></Route>
-        <Route path={ROUTES.user} component={User}></Route>
         <Route path={ROUTES.pet('')} component={Pet}></Route>
         <Route path={ROUTES.addpet} component={AddPet}></Route>
         <Route path={ROUTES.comparison} component={Comparison}></Route>
--- src/shared/constants/routes.constants.ts
+++ src/shared/constants/routes.constants.ts
@@ -1,6 +1,5 @@
 export const ROUTES = {
   home: '/',
-  user: '/user/:id',
   addpet: '/addpet',
   result: '/result',
   pet: (id: string) => `/pet/${id}`,
