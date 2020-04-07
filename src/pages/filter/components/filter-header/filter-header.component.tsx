868f7625e5bf9efafbd8521135d56ef5fe6cfad5
Pavlo Kantsir <34620231+Pashkaua@users.noreply.github.com>
2020-04-07 14:55:44 +0300

    change comparison logic in are page, change max val range in filter page

--- src/pages/filter/components/filter-header/filter-header.component.tsx
+++ src/pages/filter/components/filter-header/filter-header.component.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-// import Logo from '../../../../images/paw-solid.svg';
 import styles from './filter-header.module.scss';
 import { Logo } from '../../../../shared/logo/logo.component';
 
@@ -7,10 +6,8 @@ export const FilterHeader: React.FC = () => {
   return (
     <div className={styles.header}>
       <div className={styles.filter_logo__container}>
-        {/* <img className={styles.filter__logo} src={Logo} alt="pet-logo" /> */}
         <Logo />
-        <div className="logo"></div>
-        <span>МійПес </span>
+        <span>МійПес</span>
       </div>
       <h1 className={styles.filter__header}>Фільтр</h1>
     </div>
