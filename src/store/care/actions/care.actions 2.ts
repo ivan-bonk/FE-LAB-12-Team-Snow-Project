61f3a5b3069f0cbb3d4bb473fc058dec9206baab
Pavlo Kantsir <34620231+Pashkaua@users.noreply.github.com>
2020-04-11 21:45:15 +0300

    delete duplicate files

--- src/store/care/actions/care.actions 2.ts
+++
@@ -1,8 +0,0 @@
-import { createAsyncAction } from 'typesafe-actions';
-import { Pet } from '../reducers/reducers.interfaces';
-
-export const getPets = createAsyncAction('GET_PETS_REQUEST', 'GET_PETS_SUCCESS', 'GET_PETS_FAILTURE')<
-  void,
-  Pet,
-  string
->();
