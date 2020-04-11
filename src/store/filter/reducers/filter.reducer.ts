2dd832b9f41d79c47e9f0952bcebbddb0db5c9b8
Pavlo Kantsir <34620231+Pashkaua@users.noreply.github.com>
2020-04-11 16:54:03 +0300

    change default value to null in filter reducer

--- src/store/filter/reducers/filter.reducer.ts
+++ src/store/filter/reducers/filter.reducer.ts
@@ -6,18 +6,7 @@ export type ProfileAction = ActionType<typeof Actions>;
 
 type State = Data | null;
 
-const initialState: Data = {
-  timeWolk: '40',
-  moneyPerMonth: '360',
-  timePerMonth: '0',
-  securityLevel: '20',
-  petSize: '0',
-  defaultValue: '0',
-  easyToTrain: false,
-  family: false,
-  apartment: false,
-  allergy: false,
-};
+const initialState: Data | null = null;
 
 const filterReducer = createReducer<State, ProfileAction>(initialState).handleType(
   'FILTER',
