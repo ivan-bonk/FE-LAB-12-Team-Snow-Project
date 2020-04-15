c9692359410dfe7f5f906e7b83dfac41aaec3f89
Oksana Khabrovska <o.khabrovska@gmail.com>
2020-04-15 12:53:58 +0400

    Add error-handling to comparison page

--- src/pages/comparison/comparison.component.tsx
+++ src/pages/comparison/comparison.component.tsx
@@ -1,10 +1,11 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, Fragment } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { GetPetsById, clearListToCompare } from 'store/comparison/actions/comparison.action';
 import { addComparisonPet } from 'store/comparison/actions/comparison-handler.action';
 import { ComparisonDisplay } from './components/comparison-display/comparison-display.component';
 import { Empty } from './components/empty/empty.component';
 import { LoadingSpinner } from 'shared/components/loading-spinner/loading-spinner';
+import {ErrorHandling} from 'shared/components/error-handling/error-handling.component';
 
 import style from './comparison.module.scss';
 
@@ -19,6 +20,8 @@ export const Comparison: React.FC<ComparisonProps> = props => {
   const dispatch = useDispatch();
 
   const pets: PetsToCompareList[] = useSelector((state: RootState) => state.comparison.petsToCompare);
+  const isLoading: boolean = useSelector((state: RootState) => state.comparison.loading);
+  const isError: boolean = !!useSelector((state: RootState) => state.comparison.error);
   const idsToCompare: string[] = useSelector((state: ComparisonState) => state.comparisonHandler.idsToCompare);
 
   ids.split('-').forEach((el, index) => {
@@ -36,16 +39,21 @@ export const Comparison: React.FC<ComparisonProps> = props => {
   }, [ids]);
 
   // Todo: Add cool animated loader component @ Bonk I.
-  const loading = pets.length > 0 ? null : <LoadingSpinner />;
+  const loading = isLoading ? <LoadingSpinner /> : null;
+  const error = isError ? <ErrorHandling/> : null;
 
   const isEmpty = ids ? loading : <Empty />;
 
   return (
     <div className={style.comparisonPage}>
       <Logo />
-      <h1 className={style.comparisonPage__header}>Порівняння</h1>
-      {isEmpty}
-      {pets.length > 0 && <ComparisonDisplay ids={ids} pets={pets} />}
+      {isError ? <ErrorHandling/> : (
+        <>
+          <h1 className={style.comparisonPage__header}>Порівняння</h1>
+          {isEmpty}
+          {pets.length > 0 && <ComparisonDisplay ids={ids} pets={pets} />}
+        </>
+      )}
     </div>
   );
 };
