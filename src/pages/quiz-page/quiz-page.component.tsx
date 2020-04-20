872040d68e8bec0bf44a88e6b5c4360b48ea3901
Pavlo Kantsir <34620231+Pashkaua@users.noreply.github.com>
2020-04-20 20:03:25 +0300

    change validation

--- src/pages/quiz-page/quiz-page.component.tsx
+++ src/pages/quiz-page/quiz-page.component.tsx
@@ -90,7 +90,10 @@ export const QuizPage: React.FC = () => {
             />
             {errors.mealNumber && <p className={styles.error}>Будь-ласка вкажіть кількість прийомів їжі на день</p>}
 
-            <MealInput name={'mealWeight'} refAttribute={register({ validate: value => parseInt(value) > 0 })} />
+            <MealInput
+              name="mealWeight"
+              refAttribute={register({ required: true, validate: value => value > 0 && !isNaN(value) })}
+            />
             {errors.mealWeight && <p className={styles.error}>Будь-ласка вкажіть вагу порції в грамах</p>}
 
             <Radio
