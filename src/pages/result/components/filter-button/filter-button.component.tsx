c3f72ed41f139ea8aba642ba478963c24f235038
Danyil <dkichalo@gmail.com>
2020-04-21 13:19:12 +0300

    Hide 'clear filter' button when no one filter value applyed

--- src/pages/result/components/filter-button/filter-button.component.tsx
+++ src/pages/result/components/filter-button/filter-button.component.tsx
@@ -21,14 +21,26 @@ export const FilterButton: React.FC<FilterButtonProps> = props => {
     }
   };
 
-  return (
-    <div className={styles.filterButtonsContainer}>
-      <Link to={ROUTES.filter} className={`${styles.filterButtonsContainer__filterButton} ${styles.button}`}>
-        <span className={styles.buttonText}>Застосувати фільтр {props.numberOfFilters}</span>
-      </Link>
-      <div className={`${styles.filterButtonsContainer__clearFilterButton} ${styles.button}`} onClick={onDivClick}>
-        <span className={styles.buttonText}>Скинути фільтр</span>
-      </div>
-    </div>
-  );
+  const renderButtons = () => {
+    if (props.numberOfFilters) {
+      return (
+        <>
+          <Link to={ROUTES.filter} className={`${styles.filterButtonsContainer__filterButton} ${styles.button}`}>
+            <span className={styles.buttonText}>Застосувати фільтр {props.numberOfFilters}</span>
+          </Link>
+          <div className={`${styles.filterButtonsContainer__clearFilterButton} ${styles.button}`} onClick={onDivClick}>
+            <span className={styles.buttonText}>Скинути фільтр</span>
+          </div>
+        </>
+      );
+    } else {
+      return (
+        <Link to={ROUTES.filter} className={`${styles.filterButtonsContainer__filterButton} ${styles.button}`}>
+          <span className={styles.buttonText}>Застосувати фільтр {props.numberOfFilters}</span>
+        </Link>
+      );
+    }
+  };
+
+  return <div className={styles.filterButtonsContainer}>{renderButtons()}</div>;
 };
