81f729b47f81dddc8037db033936141a8ecc9338
Danyil <dkichalo@gmail.com>
2020-04-24 16:42:08 +0300

    Add test for result reducer

---
+++ src/store/result/mocks.ts
@@ -0,0 +1,64 @@
+import { PetProfile } from 'shared/interfaces/pet-profile.interface';
+
+export const pet: PetProfile = {
+  characteristics: {
+    popularity: 4,
+    training: 5,
+    size: 3,
+    brain: 5,
+    security: 3,
+    childFriendly: 5,
+    agility: 4,
+    problematic: 4,
+  },
+
+  observations: {
+    country: 'США',
+    lifetime: '13-15 років',
+    height: '51-58 см',
+    weight: '25-32 кг',
+    price: '500 - 1200 $',
+    carePrice: 1600,
+    careTime: 2,
+    careComplications: 'average',
+    walkNumber: 2,
+    mealNumber: 2,
+    mealWeight: 400,
+    medCheckUp: 2,
+  },
+
+  ownerInfo: {
+    easyTrain: true,
+    familyFit: true,
+    apartmentFit: true,
+    hypoallergenicity: false,
+  },
+
+  additionalInfo: {
+    specialties: [
+      'Порода собак ауссі має густу шерсть, яка потребує вічісування 2-3 рази в тиждень. Підрізаті кігті три рази на місяць, купати собаку один раз в два тіжні або частіше, за потребою. Очі очищають від відкладень щодня, вуха чистять три рази в тиждень.',
+    ],
+    issues: [
+      'Дісплазія кульшового суглобу - спадкове захворювання',
+      'Дісплазія ліктя - це такоже спадкове захворювання, характерне для собак великих порід',
+      'Епілепсія - може передаватіся у спадок',
+      'Глухота',
+      'Остеохондроз',
+      'Прогресуюча атрофія сітківки',
+      'Катаракта',
+      'Алергія',
+    ],
+    about:
+      'Собака породи ауссі має довгу шерсть. Морда витягнута, голова овальної форми, вуха середньої довжини, трикутні, звисають з боків голови. Статура міцна, мускулиста. Хвіст довгий. Пухнаста.',
+  },
+
+  imgUrl: [
+    'https://res.cloudinary.com/pet-image-api/image/upload/v1586169401/Pets/australian-shepherd/australian-shepherd-puppys_gdwfsl.jpg',
+    'https://res.cloudinary.com/pet-image-api/image/upload/v1586169401/Pets/australian-shepherd/25_su1w9i.jpg',
+    'https://res.cloudinary.com/pet-image-api/image/upload/v1586169401/Pets/australian-shepherd/aussie_eezpmt.jpg',
+  ],
+
+  _id: '5e8c4ffb4ed5f230f4c261f9',
+
+  breed: 'Австралійська вівчарка (Ауссі)',
+};
