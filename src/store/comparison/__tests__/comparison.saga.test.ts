73f2828a60dfdd15e78653f5a9f71fc471d3b3c4
Ivan Bonk <44017234+IvanBonk@users.noreply.github.com>
2020-04-25 14:10:51 +0300

    Update comparison.saga.test.ts

--- src/store/comparison/__tests__/comparison.saga.test.ts
+++ src/store/comparison/__tests__/comparison.saga.test.ts
@@ -17,36 +17,36 @@ describe('test fetch pets by id function', () => {
   });
 });
 
-// describe('test comparison saga - error', () => {
-//   it('should call api and dispatch failure action', async () => {
-//     const dispatched: any[] = [];
-//     const result = await runSaga(
-//       {
-//         dispatch: action => dispatched.push(action),
-//       },
-//       getPetsByIdWorker,
-//       {
-//         type: GetPetsById.request,
-//         payload: ['aaa', 'aaa'],
-//       },
-//     ).toPromise();
-//     expect(dispatched).toEqual([GetPetsById.failure('Magic error')]);
-//   });
-// });
+describe('test comparison saga - error', () => {
+  it('should call api and dispatch failure action', async () => {
+    const dispatched: any[] = [];
+    const result = await runSaga(
+      {
+        dispatch: action => dispatched.push(action),
+      },
+      getPetsByIdWorker,
+      {
+        type: GetPetsById.request,
+        payload: ['aaa', 'aaa'],
+      },
+    ).toPromise();
+    expect(dispatched).toEqual([GetPetsById.failure('Magic error')]);
+  });
+});
 
-// describe('test comparison saga - success', () => {
-//   it('should call api and dispatch success action', async () => {
-//     const dispatched: any[] = [];
-//     const result = await runSaga(
-//       {
-//         dispatch: action => dispatched.push(action),
-//       },
-//       getPetsByIdWorker,
-//       {
-//         type: GetPetsById.request,
-//         payload: ['5e8c4ffb4ed5f230f4c261fb', '5e8c4ffb4ed5f230f4c261f9'],
-//       },
-//     ).toPromise();
-//     expect(dispatched).toEqual([GetPetsById.success(petsToCompare)]);
-//   });
-//});
+describe('test comparison saga - success', () => {
+  it('should call api and dispatch success action', async () => {
+    const dispatched: any[] = [];
+    const result = await runSaga(
+      {
+        dispatch: action => dispatched.push(action),
+      },
+      getPetsByIdWorker,
+      {
+        type: GetPetsById.request,
+        payload: ['5e8c4ffb4ed5f230f4c261fb', '5e8c4ffb4ed5f230f4c261f9'],
+      },
+    ).toPromise();
+    expect(dispatched).toEqual([GetPetsById.success(petsToCompare)]);
+  });
+});
